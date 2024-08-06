ebe782f5b994c001d54d5602eb81a0031b342ac7
Mylyn-1122 <26zhengmylyn@gmail.com>
2024-08-06 11:43:52 -0700

    Navigation babyeyygesbfjdgfjlhjbdfuyeyf

---
+++ Assets/Scripts/Navigate/Navigate_Value.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Navigate_Value : MonoBehaviour
+{
+    [SerializeField] Vector3 direction;
+
+    public Vector3 getDirection()
+    {
+        return direction;
+    }
+
+}
