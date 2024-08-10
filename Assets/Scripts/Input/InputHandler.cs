d93cf28e10827bbe82760a976c80f6523c6e7e5d
Mylyn-1122 <26zhengmylyn@gmail.com>
2024-08-09 22:27:16 -0700

    1.0.1

--- Assets/Scripts/Input/InputHandler.cs
+++ Assets/Scripts/Input/InputHandler.cs
@@ -5,29 +5,28 @@ public class InputHandler : MonoBehaviour
 {
 
     private Camera _mainCamera;
-    private GameObject _cameraTarget;
-    private Navigate_Value _navigate_value;
-    private GameObject _navigator;
-    private Vector3 _direction;
+
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _mainCamera = Camera.main;
-        _cameraTarget = GameObject.Find("/CameraTarget");
+        //_cameraTarget = GameObject.Find("/CameraTarget");
     }
 
     public void OnClick(InputAction.CallbackContext context)
     {
         if (!context.started) return;
-
+        
         var rayhit = Physics2D.GetRayIntersection(_mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue()));
         if (!rayhit.collider) return;
 
         //_navigate_value = rayhit.collider.gameObject.GetComponent<Navigate_Value>();
-
-        _direction = rayhit.collider.gameObject.GetComponent<Navigate_Value>().getDirection();
-
-        _cameraTarget.transform.position = _direction;
+        
+        
+        rayhit.collider.gameObject.GetComponent<Object_Action>().getAction();
+        
     }
 
     // Update is called once per frame
