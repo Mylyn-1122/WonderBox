e9224e886b1a16ac58c5bdd6a868648219f9c4d4
Siyou-Wang <SiyouWang1@gmail.com>
2024-12-30 23:27:35 -0800

    New inventoryitem might be repeated script, checking tutorial tomorrow

---
+++ Assets/Scripts/Room 1/Item.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using System.Collections;
+using System.Collections.Generic;
+
+
+[CreateAssetMenu(menuName = "Scriptable object/Item")]
+public class Item : ScriptableObject
+{
+    public Tilemap tile;
+    public ItemType type;
+    public ActionType actiontype;
+    public Vector2Int range = new Vector2Int(5, 4);
+
+    [Header("Only UI")]
+    public bool stackable = true;
+
+    [Header("Both")]
+    public Sprite image;
+
+
+    public enum ItemType {
+        Key,
+        Puzzle
+
+    }
+
+    public enum ActionType {
+        Unlock,
+        JustStore
+    }
+
+}
