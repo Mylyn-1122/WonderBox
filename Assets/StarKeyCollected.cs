636a744f03a20d6491be16dfcb529e36d0b1d0e6
Siyou-Wang <SiyouWang1@gmail.com>
2025-10-21 21:44:30 -0700

    adjusted star key script

--- Assets/StarKeyCollected.cs
+++ Assets/StarKeyCollected.cs
@@ -16,14 +16,17 @@ public class StarKeyCollected : MonoBehaviour
         starKeyB = GameObject.Find("starKey_Blue").GetComponent<SpriteRenderer>();
         starB = GameObject.Find("starKey_Blue");
         starKeyB.enabled = false;
+        starKeyB.GetComponent<BoxCollider2D>().enabled = false;
         //starB.SetActive(false);
         starKeyY = GameObject.Find("starKey_Yellow").GetComponent<SpriteRenderer>();
         starY = GameObject.Find("starKey_Yellow");
         starKeyY.enabled = true;
+    
         //starY.SetActive(true);
         starKeyR = GameObject.Find("starKey_Red").GetComponent<SpriteRenderer>();
         starR = GameObject.Find("starKey_Red");
         starKeyR.enabled = false;
+        starKeyR.GetComponent<BoxCollider2D>().enabled = false;
         //starR.SetActive(false);
     }
 
@@ -34,23 +37,28 @@ public class StarKeyCollected : MonoBehaviour
 
         if (RPGManagerR1.getVictor()&&!Collectable.redC) {
             starKeyR.enabled = true;
+            starKeyR.GetComponent<BoxCollider2D>().enabled = true;
             starR.SetActive(true);
 
         }
         if (StainedGlassWindowGame.complete&&!Collectable.blueC) {
             starKeyB.enabled = true;
+            starKeyB.GetComponent<BoxCollider2D>().enabled = true;
             starB.SetActive(true);
         }
         if (Collectable.redC) {
             starKeyR.enabled = false;
+            starKeyR.GetComponent<BoxCollider2D>().enabled = false;
             starR.SetActive(false);
         }
         if (Collectable.blueC) {
             starKeyB.enabled = false;
+            starKeyB.GetComponent<BoxCollider2D>().enabled = false;
             starB.SetActive(false);
         }
         if (Collectable.yellowC) {
             starKeyY.enabled = false;
+            starKeyY.GetComponent<BoxCollider2D>().enabled = false;
             starY.SetActive(false);
         }
        
